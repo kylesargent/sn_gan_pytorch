d5c4e5c8c062624432af8e91d511456325d6e004
KyleSargent <kylesargent8@gmail.com>
2019-03-03 18:08:31 -0500

    Added spectral layers module

---
+++ spectral_layers.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def max_singular_value(weight, u, Ip):
+    assert(Ip >= 1)
+    
+    _u = u
+    for _ in range(Ip):
+        _v = F.normalize(torch.mm(_u, weight), p=2, dim=1).detach()
+        _u = F.normalize(torch.mm(_v, weight.transpose(0,1)), p=2, dim=1).detach()
+    sigma = torch.sum(F.linear(_u, weight.transpose(0,1)) * _v)
+    return sigma, _u
+
+class SNLinear(nn.Linear):
+    
+    def __init__(self, in_features, out_features, bias=True, init_u = None):
+        super(SNLinear, self).__init__(
+            in_features, out_features, bias
+        )
+        self.Ip = 1
+        if init_u is not None:
+            self.u = init_u
+        else:
+            self.u = torch.randn(1, out_features)
+        
+    @property
+    def W_bar(self):
+        sigma, u = max_singular_value(self.weight, self.u, self.Ip)
+
+        print(sigma, u)
+
+        self.u = u
+        return self.weight / sigma
+
+    def forward(self, x):
+        return F.linear(x, self.W_bar, self.bias)
+    
+class SNConv2d(nn.Conv2d):
+    
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, init_u=None):
+        super(SNConv2d, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias
+        )
+        self.Ip = 1
+        if init_u is not None:
+            self.u = init_u
+        else:
+            self.u = torch.randn(1, out_channels)
+        
+    @property
+    def W_bar(self):
+        w = self.weight
+        w = w.view(w.shape[0], -1)
+        
+        sigma, u = max_singular_value(w, self.u, self.Ip)
+
+        # print(sigma, u)
+
+        self.u = u
+        return self.weight / sigma
+    
+    def forward(self, x):
+        return F.conv2d(
+            x, 
+            self.W_bar,
+            bias=self.bias,
+            stride=self.stride, 
+            padding=self.padding, 
+            dilation=self.dilation,
+            groups=self.groups
+        )
